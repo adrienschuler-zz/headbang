f9f5b9ee028a934a4fb115852f8009ce183b0b0b
Adrien Schuler <>
2018-02-05 18:51:59 +0100

    sample task

---
+++ tasks/consolidate.py
@@ -0,0 +1,12 @@
+from app import Config, Model, Log
+
+
+class Consolidate:
+
+    def __init__(self):
+        pass
+
+    def places(self):
+        ''' Consolidate Foursquare venues and Google places
+        '''
+        Log.info('Consolidate Places')
